1721472be54fc6dd9e69470df7ceb9d9c66d1275
Hưng Nguyễn Duy <nduyhung271090@gmail.com>
2020-05-12 16:19:58 +0700

    test 2

--- AspNetCoreApp.Web/Startup.cs
+++ AspNetCoreApp.Web/Startup.cs
@@ -35,7 +35,6 @@ namespace AspNetCoreApp.Web
             }
             else
             {
-                string a = "";
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
